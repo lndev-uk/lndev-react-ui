fcbfc86fc95e0611347dd00435171ecee287b688
Luis Nesi <lenesi@hotmail.com>
2019-12-06 00:34:57 +0000

    export animation hook

--- src/index.js
+++ src/index.js
@@ -3,3 +3,9 @@ export { default as Loader } from './components/Loader';
 export { default as Badge } from './components/Badge';
 export { default as Button } from './components/Button';
 export { default as Notification } from './components/Notification';
+
+import useAnimation from './hooks/useAnimation';
+
+export const hooks = {
+  useAnimation
+};
